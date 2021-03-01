a7470ef5cab4cbb0ab526f86a93a3f5cb0da095d
Viniciusalopes <suporte@viniciusalopes.com.br>
2021-03-01 00:07:46 -0300

    Diagrama de Classes e link para o repositório. Spring initializr

---
+++ matriculas/src/main/java/br/com/vovolinux/matriculas/MatriculasApplication.java
@@ -0,0 +1,13 @@
+package br.com.vovolinux.matriculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculasApplication.class, args);
+	}
+
+}
---
+++ matriculas/src/test/java/br/com/vovolinux/matriculas/MatriculasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vovolinux.matriculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
